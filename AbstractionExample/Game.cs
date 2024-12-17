c1e69c6e02313cdaeb80fdc7e56e29507479546c
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-12-17 18:18:42 +0300

    Добавлено описание о полиморфизме

---
+++ AbstractionExample/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterStrike
+{
+    internal class Game
+    {
+        public void StartGame(IPlayer player1, IPlayer player2)
+        {
+            /*Логика игры*/
+
+            //Скажем стреляет 1 игрок
+            
+            player1.Shoot();
+            
+            //Стреляет второй игрок
+            player2.Shoot();
+        }
+    }
+}
--- AbstractionExample/Program.cs
+++ AbstractionExample/Program.cs
@@ -1,4 +1,9 @@
-﻿IPlayer player = new BotPlayer();
+﻿using CounterStrike;
+
+IPlayer player = new BotPlayer();
 
 IPlayer terrorist = new TerroristPlayer("John");
 IPlayer contrTerrorist = new ContrTerroristPlayer("Adam");
+
+//На момент начала игры мы даже не знаем кто с кем играет: человек с человеком, человек с машиной или машина с машиной
+new Game().StartGame(player, terrorist);
