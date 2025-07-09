c4d3481262a55545cdd57f95dab75843d29e3b57
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2025-07-09 12:59:49 +0300

    Исправлена абстракция.

---
+++ OOP/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿//Animal animal = new Animal();
+
+Animal dog = new Dog();
+dog.Speak();    // Выведет: Собака лает
+
+// Абстрактный класс: нельзя создать экземпляр, только наследовать
+abstract class Animal
+{
+    // Абстрактный метод — без реализации
+    public abstract void Speak();
+}
+
+// Конкретный класс, реализующий абстракцию
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Собака лает");
+    }
+}
